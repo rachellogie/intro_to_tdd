e07c756ecfc893d2ce73603e2acc2deca91d0a56
rachellogie <rachel.logie1@gmail.com>
2014-03-11 15:55:21 -0600

    numeric_calculator: added before-do block to implement setup

--- spec/numeric_calculator_spec.rb
+++ spec/numeric_calculator_spec.rb
@@ -2,45 +2,43 @@ require "numeric_calculator"
 
 describe "NumericCalculator" do
 
-  it "has an add method that returns the sum of two numbers" do
-    calc = NumericCalculator.new
+  before(:each) do
+    @calc = NumericCalculator.new
+  end
 
+  it "has an add method that returns the sum of two numbers" do
     actual = 9
 
-    expected = calc.add(5, 4)
+    expected = @calc.add(5, 4)
 
     expect(actual). to eq expected
   end
 
   it "has a subtract method that returns difference of two numbers" do
-    calc = NumericCalculator.new
-
     actual = 45
 
-    expected = calc.subtract(52, 7)
+    expected = @calc.subtract(52, 7)
 
     expect(actual). to eq expected
   end
 
   it "can save things to the calc's memory and get it later" do
-    calc = NumericCalculator.new
-    calc.save(89)
+    @calc.save(89)
 
     actual = 89
 
-    expected = calc.get
+    expected = @calc.get
 
     expect(actual). to eq expected
   end
 
   it "can clear the memory and then get method returns 0" do
-    calc = NumericCalculator.new
-    calc.save(100)
-    calc.clear
+    @calc.save(100)
+    @calc.clear
 
     actual = 0
 
-    expected = calc.get
+    expected = @calc.get
 
     expect(actual). to eq expected
   end
